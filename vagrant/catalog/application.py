b9e36831913b9047cf976992fec14ccbd56b402d
Matt Weeks <m_weeks@hotmail.co.uk>
2016-05-26 07:50:02 +0100

    Add custom 404 page.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -62,6 +62,11 @@ def showGames():
 # Login pages
 
 
+# Custom error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
