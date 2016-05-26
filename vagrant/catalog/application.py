46bb5af8b5109b2fbedc3e1baa6c4a590df5e79b
Matt Weeks <m_weeks@hotmail.co.uk>
2016-05-26 08:02:52 +0100

    Add JSON for all games.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -21,23 +21,32 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # JSON pages
+# JSON for all developers
 @app.route('/JSON')
 @app.route('/developers/JSON')
 def developersJSON():
     developers = session.query(Developer).all()
     return jsonify(developers=[r.serialize for r in developers])
 
+# JSON for all a single developer's games
 @app.route('/developer/<int:developer_id>/JSON')
 @app.route('/developer/<int:developer_id>/games/JSON')
 def developerGamesJSON(developer_id):
     developer = session.query(Developer).filter_by(id=developer_id).one()
     games = session.query(Game).filter_by(d_id=developer_id).all()
-    return jsonify(Games=[i.serialize for i in games])
+    return jsonify(games=[i.serialize for i in games])
 
+# JSON for single game
 @app.route('/developer/<int:developer_id>/game/<int:game_id>/JSON')
 def gameJSON(developer_id, game_id):
     game = session.query(Game).filter_by(id=game_id).one()
     return jsonify(game=game.serialize)
+
+# JSON for all games
+@app.route('/games/JSON')
+def gamesJSON():
+    games = session.query(Game).all()
+    return jsonify(games=[i.serialize for i in games])
 
 
 # HTML pages
